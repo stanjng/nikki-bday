1c2bd9e74e36edb689fafd4808df771b05fef931
stanjng <sljadverts@gmail.com>
2020-05-05 13:30:02 -0400

    made nikkis website via ga template

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,42 @@
 import React from 'react'
-import bdayCard from '../../public/nikki-card.png'
-import cardText from '../../public/card-text.png'
+import bdayCard from '../../resources/nikki-card.png'
+import cardText from '../../resources/card-text.png'
 import './App.scss'
 import Flippy, { FrontSide, BackSide } from 'react-flippy'
 
 const App = () => {
   return (
-    <div className="App">
+    <div
+      className="App"
+    >
       <Flippy
         flipOnHover={false} // default false
         flipOnClick={true} // default false
         flipDirection="horizontal" // horizontal or vertical
+        style={{
+          display: 'flex',
+          alignItems: 'center'
+        }}
       >
         <FrontSide
           style={{
             boxShadow: 'none'
           }}
         >
-          <img className="card-height" src={process.env.PUBLIC_URL + bdayCard} alt="Happy Birthday My Love!" />
+          <img className="card-height" src={bdayCard} alt="Happy Birthday My Love!" />
         </FrontSide>
         <BackSide
           style={{
             boxShadow: 'none'
           }}>
-          <img className="card-height" src={process.env.PUBLIC_URL + cardText} alt="Happy Birthday My Love - Text!" />
+          <img className="card-height" src={cardText} alt="Happy Birthday My Love - Text!" />
         </BackSide>
       </Flippy>
+      <footer>
+      Love you Nikki, Just click the card to flip it!
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
